a3ba28f5baa0cb4d671b86eafd1a74bfc9c9a6ca
Claudio Gonzalez Hernandez <40004826+Claudio-2019@users.noreply.github.com>
2021-05-19 16:02:06 -0600

    Landing page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TecnologiesComponent } from './tecnologies/tecnologies.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutUsComponent,
+    TecnologiesComponent,
+    PortafolioComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
