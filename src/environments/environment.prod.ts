4d2ad7068c2bca44a74716dcef6d251413e4e172
Claudio josue Gonzalez <claudiogh33@gmail.com>
2021-05-20 13:40:59 -0600

    enviroment handle

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCERCSj4S8-b_SM3aAXw979bkmsImFfyt8",
+    authDomain: "portafolio-web-claudio.firebaseapp.com",
+    projectId: "portafolio-web-claudio",
+    storageBucket: "portafolio-web-claudio.appspot.com",
+    messagingSenderId: "1067709840749",
+    appId: "1:1067709840749:web:679fe966ed82c63574ee64",
+    measurementId: "G-LYQ7XV6EEB"
+  }
 };
