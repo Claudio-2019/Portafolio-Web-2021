3f91c19dcf4c13d514194e39f726ae0613b07d8d
Claudio Gonzalez Hernandez <40004826+Claudio-2019@users.noreply.github.com>
2021-05-19 23:07:23 -0600

    pages implemented, missing portafolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { TecnologiesComponent } from './tecnologies/tecnologies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'Contact-Me',component:ContactUsComponent},
+  {path:'Web-Proyects',component:PortafolioComponent},
+  {path:'My-Technology-Strenght',component:TecnologiesComponent},
+  {path:'About-Me',component:AboutUsComponent},
+  {path:'**',redirectTo:'About-Me'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
